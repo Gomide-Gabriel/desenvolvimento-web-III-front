85d29830257fbedf3a05eaca21cc8732f5590e58
Gomide-Gabriel <nephilin196@gmail.com>
2026-09-02 10:25:32 -0300

    att

--- src/Pages/CadastroCliente.jsx
+++ src/Pages/CadastroCliente.jsx
@@ -20,37 +20,37 @@ function CadastroCliente() {
     return (
         <main className="pagina-clientes">
 
- <h1>Cadastrar novo cliente</h1>
- <form className="formulario-cliente" onSubmit={cadastrarCliente}>
-    <label htmlFor="nome">Nome</label>
-        <input  id="nome" type="text" value={nome}
-            onChange={(evento) => 
-                setNome(evento.target.value)}
-                required
-        />
-    <label htmlFor="cpf">CPF</label>
-        <input id="cpf" type="text" value={cpf}
-            onChange={(evento) => 
-                setCpf(evento.target.value)}
-                maxLength="11"
-                required
-        />
+        <h1>Cadastrar novo cliente</h1>
+        <form className="formulario-cliente" onSubmit={cadastrarCliente}>
+            <label htmlFor="nome">Nome</label>
+                <input  id="nome" type="text" value={nome}
+                    onChange={(evento) => 
+                        setNome(evento.target.value)}
+                        required
+                />
+            <label htmlFor="cpf">CPF</label>
+                <input id="cpf" type="text" value={cpf}
+                    onChange={(evento) => 
+                        setCpf(evento.target.value)}
+                        maxLength="11"
+                        required
+                />
 
-    <label htmlFor="telefone">Telefone</label>
-        <input id="telefone" type="text" value={telefone}
-            onChange={(evento) => 
-                setTelefone(evento.target.value)}
-        />
-    <label htmlFor="email">E-mail</label>
-        <input id="email" type="email" value={email}
-            onChange={(evento) => 
-                setEmail(evento.target.value)}
-        />
-        <button type="submit">Cadastrar cliente</button>
- </form>
- <Link to="/clientes">Voltar para Gerenciamento de Clientes</Link>
+            <label htmlFor="telefone">Telefone</label>
+                <input id="telefone" type="text" value={telefone}
+                    onChange={(evento) => 
+                        setTelefone(evento.target.value)}
+                />
+            <label htmlFor="email">E-mail</label>
+                <input id="email" type="email" value={email}
+                    onChange={(evento) => 
+                        setEmail(evento.target.value)}
+                />
+                <button type="submit">Cadastrar cliente</button>
+        </form>
+        <Link to="/clientes">Voltar para Gerenciamento de Clientes</Link>
 
- </main>
+        </main>
  )
 }
 export default CadastroCliente
